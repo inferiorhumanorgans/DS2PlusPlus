712e7785f16fe6d4b9c1736cef5ab2d9b2384c4a
Alex Zepeda <alex@inferiorhumanorgans.com>
2014-09-04 15:55:57 -0700

    ds2-dump: Add list-ecus, list-families, and list-operations. Update DL.

--- ds2-dump/ds2-dump.cpp
+++ ds2-dump/ds2-dump.cpp
@@ -1,8 +1,13 @@
+#include <sys/time.h>
 #include <unistd.h>
+
+#include <stdexcept>
 
 #include <QDebug>
 #include <QCommandLineParser>
 #include <QSharedPointer>
+#include <QFile>
+#include <QDate>
 
 #include <ds2packet.h>
 #include <manager.h>
@@ -29,29 +34,41 @@ void DataCollection::run()
 
     ManagerPtr dbm(new Manager(parser));
 
-    QCommandLineOption setEcuOption(QStringList() << "e" << "ecu", "The ECU to operate on.", "ecu");
+    QCommandLineOption reloadJsonOption(QStringList() << "r" << "reload" << "load", "Load JSON data into SQL db");
+    parser->addOption(reloadJsonOption);
+
+    QCommandLineOption setEcuOption(QStringList() << "e" << "ecu", "The ECU to operate on (family name or numerical address).", "ecu");
     parser->addOption(setEcuOption);
 
-    QCommandLineOption listEcuOption("list-families", "The known ECU families.");
-    parser->addOption(listEcuOption);
-
-    QCommandLineOption detectEcuOption(QStringList() << "p" << "probe", "Probe an ECU for its identity.");
+    QCommandLineOption detectEcuOption(QStringList() << "p" << "probe", "Probe an ECU at <ecu> for its identity.");
     parser->addOption(detectEcuOption);
 
-    QCommandLineOption runJobOption(QStringList() << "j" << "run-job", "Probe an ECU at <address> for its identity.", "job");
+    QCommandLineOption runJobOption(QStringList() << "j" << "run-job", "Probe an ECU at <ecu> for its identity.", "job");
     parser->addOption(runJobOption);
 
-    QCommandLineOption iterateOption(QStringList() << "n" << "iterate", "Iterate <n> number of times over the job.", "n");
+    QCommandLineOption iterateOption(QStringList() << "n" << "iterate", "Iterate <n> number of times.", "n");
     parser->addOption(iterateOption);
 
-    QCommandLineOption datalogOption(QStringList() << "d" << "data-log", "Create a CSV log, write until interrupted.");
+    QCommandLineOption datalogOption(QStringList() << "d" << "data-log", "Create a CSV log, write until interrupted.", "ecu-jobs-and-results");
     parser->addOption(datalogOption);
 
+    QCommandLineOption listFamiliesOption("list-families", "Print the known ECU families.");
+    parser->addOption(listFamiliesOption);
+
+    QCommandLineOption listEcuOption("list-ecus", "Print the known ECUs for a given family.", "family");
+    parser->addOption(listEcuOption);
+
+    QCommandLineOption listOperationsOption("list-operations", "Print the known operations for a given ECU.", "uuid");
+    parser->addOption(listOperationsOption);
+
     parser->process(*QCoreApplication::instance());
+
     dbm->initializeManager();
 
     // This will rescan all the JSON files, we should be smarter about doing this.
-    dbm->initializeDatabase();
+    if (parser->isSet("reload")) {
+        dbm->initializeDatabase();
+    }
 
     if (parser->isSet("list-families")) {
         // Gross but this will initialize the hash for us.
@@ -60,6 +77,77 @@ void DataCollection::run()
         QStringList families = ControlUnit::knownFamilies();
         families.sort();
         qOut << "Known ECU families: " << families.join(", ") << endl;
+
+        emit finished();
+        return;
+    }
+
+    if (parser->isSet("list-ecus")) {
+        QHash<QString, ControlUnitPtr> ourEcus;
+
+        QString aFamily = parser->value("list-ecus");
+        if (aFamily == "ALL") {
+            ourEcus = dbm->findAllModules();
+        } else {
+            ourEcus = dbm->findAllModulesByFamily(aFamily);
+        }
+
+        int totalLen = 38+40+9;
+        qOut << qSetFieldWidth(38) << left << "UUID";
+        qOut << qSetFieldWidth(40) << left << "Name";
+        qOut << qSetFieldWidth(9)  << left << "Version";
+
+        if (aFamily == "ALL") {
+            qOut << qSetFieldWidth(8) << left << "Family";
+            totalLen += 8;
+        }
+
+        qOut << qSetFieldWidth(1) << endl;
+
+
+        qOut << qSetFieldWidth(totalLen) << qSetPadChar('-') << "" << qSetFieldWidth(1) << qSetPadChar(' ') << endl;
+
+        foreach (const ControlUnitPtr ourEcu, ourEcus) {
+            qOut << qSetFieldWidth(38) << left << ourEcu->uuid();
+            qOut << qSetFieldWidth(40) << left << ourEcu->name();
+            qOut << qSetFieldWidth(9)  << left << ourEcu->fileVersion();
+            if (aFamily == "ALL") {
+                qOut << qSetFieldWidth(8) << left << ControlUnit::familyForAddress(ourEcu->address());
+            }
+            qOut << qSetFieldWidth(1) << endl;
+        }
+        qOut << endl;
+
+        emit finished();
+        return;
+    }
+
+    if (parser->isSet("list-operations")) {
+        ControlUnitPtr ourEcu(new ControlUnit(parser->value("list-operations")));
+        QHash<QString, OperationPtr> ourOperations = ourEcu->operations();
+        qOut << "Operations for: " << ourEcu->name() << " (" << ourEcu->uuid() << ")" << endl << endl;
+
+        QHash<QString, QStringList> opsHash;
+        foreach (const OperationPtr ourOp, ourOperations.values()) {
+
+            QList<Result> results = ourOp->results().values();
+            QStringList resultNames;
+            foreach (const Result &result, results) {
+                resultNames.append(result.name());
+            }
+            resultNames.sort();
+            opsHash[ourOp->name()] = resultNames;
+        }
+
+        QStringList opNames = opsHash.keys();
+        opNames.sort();
+        foreach (const QString &ourOpName, opNames) {
+            qOut << qSetFieldWidth(11) << left << "Operation:";
+            qOut << qSetFieldWidth(1) << ourOpName << endl;
+            qOut << qSetFieldWidth(11) << "Returns:";
+            qOut << qSetFieldWidth(1) << opsHash[ourOpName].join(", ") << endl;
+            qOut << endl;
+        }
 
         emit finished();
         return;
@@ -82,15 +170,16 @@ void DataCollection::run()
                 return;
             }
         }
-    } else {
-        qErr << "A valid ECU family or address is required to proceed further." << endl;
+    } else if (!parser->isSet("data-log")) {
+        if (!parser->isSet("reload")) {
+            qErr << "A valid ECU family or address is required to proceed further." << endl;
+        }
         emit finished();
         return;
     }
 
-    DS2PlusPlus::ControlUnitPtr autoDetect(dbm->findModuleAtAddress(ecuAddress));
-
     if (parser->isSet("run-job")) {
+        DS2PlusPlus::ControlUnitPtr autoDetect(dbm->findModuleAtAddress(ecuAddress));
         if (!autoDetect.isNull()) {
             QString ourJob = parser->value("run-job");
             qOut << QString("At 0x%1 we think we have: %2").arg(ecuAddress, 2, 16, QChar('0')).arg(autoDetect->name()) << endl;
@@ -121,6 +210,7 @@ void DataCollection::run()
         }
 
     } else if (parser->isSet("probe")) {
+        DS2PlusPlus::ControlUnitPtr autoDetect(dbm->findModuleAtAddress(ecuAddress));
         if (!autoDetect.isNull()) {
             qOut << QString("At 0x%1 we think we have: %2").arg(ecuAddress, 2, 16, QChar('0')).arg(autoDetect->name()) << endl;
             DS2Response ourResponse = autoDetect->executeOperation("identify");
@@ -130,6 +220,102 @@ void DataCollection::run()
             DS2PacketPtr aResponsePacket = dbm->query(anIdentPacket);
             qDebug() << "Couldn't find a match, got this response: " << endl << *aResponsePacket << endl;
         }
+    } else if (parser->isSet("data-log")) {
+        // ECU:job-1:result,result,result
+        QStringList logSpecs = parser->values("data-log");
+        QMap<QString, DS2PlusPlus::ControlUnitPtr> ecus;
+        QMap<QString, QMap<QString, QStringList> > jobs;
+
+        foreach (const QString &spec, logSpecs) {
+            QStringList currentSpec = spec.split(":");
+
+            if (currentSpec.length() != 3) {
+                throw std::invalid_argument("Data log spec must follow the format ECU:job:result1,result2,resultn");
+            }
+
+            QString ecuName = currentSpec.at(0);
+            QString jobName = currentSpec.at(1);
+            QStringList resultsList = currentSpec.at(2).split(",");
+
+            if (!ecus.contains(ecuName)) {
+                DS2PlusPlus::ControlUnitPtr ourEcu(dbm->findModuleAtAddress(ControlUnit::addressForFamily(ecuName)));
+                if (ourEcu.isNull()) {
+                    throw std::invalid_argument(qPrintable(QString("Could not locate ECU at %1").arg(ecuName)));
+                }
+                ecus.insert(ecuName, ourEcu);
+            }
+
+
+            jobs[ecuName][jobName].append(resultsList);
+        }
+
+
+        QFile file(QString("dpp-%1.csv").arg(QDateTime::currentDateTime().toString()));
+        file.open(QIODevice::WriteOnly | QIODevice::Text);
+        QTextStream out(&file);
+
+        QStringList headers, formats;
+
+        headers << "Time";
+        formats << "s";
+
+        foreach (const QString &ecuName, ecus.keys()) {
+            QMap<QString, QStringList> ourJobs = jobs[ecuName];
+            foreach (const QString &jobName, ourJobs.keys()) {
+                QStringList ourResults = ourJobs[jobName];
+                foreach (const QString &resultName, ourResults) {
+                    headers << QString("%1:%2:%3").arg(ecuName).arg(jobName).arg(resultName);
+                    formats << "int";
+                }
+            }
+        }
+
+        out << headers.join("\t") << endl;
+        out << formats.join("\t") << endl;
+        out.setRealNumberNotation(QTextStream::FixedNotation);
+        out.setRealNumberPrecision(5);
+
+        while (true) {
+            QStringList outValues;
+            foreach (const QString &ecuName, ecus.keys()) {
+                DS2PlusPlus::ControlUnitPtr ourEcu = ecus[ecuName];
+                QMap<QString, QStringList> ourJobs = jobs[ecuName];
+
+                foreach (const QString &jobName, ourJobs.keys()) {
+                    QStringList ourResults = ourJobs[jobName];
+                    DS2Response ourResponse = ourEcu->executeOperation(jobName);
+
+                    foreach (const QString &resultName, ourResults) {
+                        QVariant ourResult = ourResponse.value(resultName);
+                        switch (ourResult.type()) {
+                        case QMetaType::UShort:
+                        case QMetaType::UInt:
+                        case QMetaType::ULong:
+                        case QMetaType::ULongLong:
+                            outValues << QString::number(ourResult.toULongLong());
+                            break;
+                        case QMetaType::Double:
+                        case QMetaType::Float:
+                            outValues << QString::number(ourResult.toDouble(), 'f', 5);
+                            break;
+                        case QMetaType::QString:
+                            outValues << ourResult.toString();
+                            break;
+                        default:
+                            outValues << "";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            double curTime = tv.tv_sec + (0.000001 * tv.tv_usec);
+            out << curTime  << "\t" << outValues.join("\t") << endl;
+            usleep(250000); // Quarter sec sleep
+        }
+        file.close();
     }
 
     emit finished();
