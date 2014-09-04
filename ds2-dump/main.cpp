4d691c3c2abed36128089c34757b3db2babe0d28
Alex Zepeda <alex@inferiorhumanorgans.com>
2014-09-04 15:58:21 -0700

    ds2-dump: Bump to v0.2.

--- ds2-dump/main.cpp
+++ ds2-dump/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    app.setApplicationVersion("0.1");
+    app.setApplicationVersion("0.2");
     app.setOrganizationDomain("inferiorhumanorgans.com");
     app.setOrganizationName("Inferior Human Organs, Inc.");
 
