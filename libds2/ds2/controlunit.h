ca8e4f743ff4953019641a63b9385efc92c3e857
Alex Zepeda <alex@inferiorhumanorgans.com>
2015-09-25 00:07:24 -0700

    ControlUnit: Add some Doxygen style comments.

--- libds2/ds2/controlunit.h
+++ libds2/ds2/controlunit.h
@@ -37,7 +37,7 @@ namespace DS2PlusPlus {
     class Manager;
 
     /*!
-     * \brief The ControlUnit class represents a module in a car.
+     * \brief An arbitrary computer module in a car that can execute operations and return results.
      */
     class ControlUnit : public QObject
     {
@@ -62,92 +62,126 @@ namespace DS2PlusPlus {
         };
 
         /*!
-         * \brief loadByUuid fetches functional info from the SQL database using the UUID as the key.
+         * \brief Fetches functional info from the SQL database using the UUID as the key.
          * \param aUuid
          */
         void loadByUuid(const QString &aUuid);
 
         /*!
-         * \brief executeOperation sends a named operation to the ECU and returns the parsed response.
+         * \brief Sends a named operation to the ECU and returns the parsed response.
          * \param aName Name of the operation
          * \return
          */
         virtual PacketResponse executeOperation(const QString &aName);
 
         /*!
-         * \brief parseOperation looks up a named operation, parses and returns a DS2Packet (typically fetched from the car)
-         * \param aName
-         * \param aPacket
-         * \return
+         * \brief Parses a BasePacket for a given operation.
+         *
+         * Typically the packet passed to this function is the packet received from a ControlUnit in response to an operation.
+         * \param aName The name of an Operation
+         * \param aPacket A BasePacket instance containing data to parse.
+         * \return A PacketResponse instance with the data parsed out into named key value pairs with the data represented by QVariant objects.
          */
         virtual PacketResponse parseOperation(const QString &aName, const BasePacketPtr aPacket);
 
         /*!
-         * \brief parseOperation parses and returns a DS2Packet (typically fetched from the car) for an Operation object
-         * \param anOperation
-         * \param aPacket
-         * \return
+         * \brief Parses a BasePacket for a given operation.
+         *
+         * Typically the packet passed to this function is the packet received from a ControlUnit in response to an operation.
+         * \param aPacket A BasePacket instance containing data to parse.
+         * \return A PacketResponse instance with the data parsed out into named key value pairs with the data represented by QVariant objects.
          */
         virtual PacketResponse parseOperation(const OperationPtr anOperation, const BasePacketPtr aPacket);
 
-        /*!
-         * \brief dppVersion
-         * \return Returns the version of the DPP JSON definition file format used to initialize this object.
-         */
+        /*! \brief Returns the \ref dppVersion property. */
         quint32 dppVersion() const;
+
+        /*! \brief The version of the DPP JSON definition file format used to initialize this object. */
         Q_PROPERTY(quint32 dppVersion MEMBER _dppVersion READ dppVersion)
 
-        /*!
-         * \brief fileVersion
-         * \return Returns the revision of the JSON definition file used to initialize this object.
-         */
+        /*! \brief Returns The \ref fileVersion property. */
         quint32 fileVersion() const;
+
+        /*! \brief The revision of the JSON definition file used to initialize this object. */
         Q_PROPERTY(quint32 fileVersion MEMBER _fileVersion READ fileVersion)
 
+        /*! \brief Returns the \ref fileLastModified property. */
         QDateTime fileLastModified() const;
+
+        /*! \brief The timestamp of the last time the source file was modified. */
         Q_PROPERTY(QDateTime fileLastModified MEMBER _fileLastModified READ fileLastModified)
 
-        /*!
-         * \brief uuid
-         * \return Returns the UUID of the JSON definition file used to initialize this object.
-         */
+        /*! \brief Returns the \ref uuid property. */
         const QString uuid() const;
+
+        /*! \brief The UUID of the JSON definition file used to initialize this object. */
         Q_PROPERTY(const QString uuid MEMBER _uuid READ uuid)
 
-        /*!
-         * \brief address
-         * \return Returns the address of this ECU
-         */
+        /*! \brief Returns the \ref address property. */
         quint8 address() const;
+
+        /*!
+         * \brief Sets the \ref address property of this ControlUnit.
+         * \param anAddress A valid address from 0-255.
+         */
         void setAddress(quint8 anAddress);
+
+        /*! \brief The (target) address of this ControlUnit. */
         Q_PROPERTY(quint8 address MEMBER _address READ address WRITE setAddress)
 
+        /*! \brief Returns the \ref family property. */
         QString family() const;
-        Q_PROPERTY(QString family MEMBER _family READ family)
-
-        bool isRoot() const;
-        Q_PROPERTY(bool isRoot READ isRoot)
 
         /*!
-         * \brief name
-         * \return Returns the plain English name of this ECU
+         * \brief The family (ex: DME, LWS, ZKE) that this ControlUnit belongs to.
          */
+        Q_PROPERTY(QString family MEMBER _family READ family)
+
+        /*! \brief Returns the \ref isRoot property */
+        bool isRoot() const;
+
+        /*! \brief True if this is the abstract root ControlUnit */
+        Q_PROPERTY(bool isRoot READ isRoot)
+
+        /*! \brief Returns the \ref name property */
         const QString name() const;
+
+        /*! \brief The name of the ControlUnit in plain English. */
         Q_PROPERTY(const QString name MEMBER _name READ name)
 
+        /*! \brief Returns the \ref partNumbers property. */
         QSet<quint64> partNumbers() const;
+
+        /*! \brief All of the part numbers that apply to this ControlUnit. */
         Q_PROPERTY(QSet<quint64> partNumbers MEMBER _partNumbers READ partNumbers)
 
+        /*! \brief Returns the \ref hardwareNumber property. */
         quint64 hardwareNumber() const;
+
+        /*! \brief The hardware version of this control unit */
         Q_PROPERTY(quint64 hardwareNumber MEMBER _hardwareNumber READ hardwareNumber)
 
+        /*! \brief Returns the \ref softwareNumber property. */
         quint64 softwareNumber() const;
+
+        /*! \brief The software version of this control unit */
         Q_PROPERTY(quint64 softwareNumber MEMBER _softwareNumber READ softwareNumber)
 
+        /*! \brief Returns the \ref codingIndex property. */
         quint64 codingIndex() const;
+
+        /*! \brief The coding index of a ControlUnit. */
         Q_PROPERTY(quint64 codingIndex MEMBER _codingIndex READ codingIndex)
 
+        /*! \brief Returns the \ref bigEndian property */
         bool bigEndian() const;
+
+        /*!
+         * \brief The endianness of this module.
+         *
+         * True if this module represents multi-word values in big-endian format, false if this module
+         * represents multi-word values in little-endian format.
+         */
         Q_PROPERTY(bool bigEndian MEMBER _bigEndian READ bigEndian)
 
         BasePacket::ProtocolType protocol() const;
@@ -161,7 +195,7 @@ namespace DS2PlusPlus {
 
     protected:
         /*!
-         * \brief resultByteToVariant handles parsing any byte sized data type
+         * \brief resultByteToVariant handles parsing byte and signed_byte data types
          * \param aPacket
          * \param aResult
          * \return
@@ -170,6 +204,14 @@ namespace DS2PlusPlus {
          */
         QVariant resultByteToVariant(const BasePacketPtr aPacket, const Result &aResult);
 
+        /*!
+         * \brief resultShortToVariant handles parsing two word data types including short and signed_short
+         * \param aPacket
+         * \param aResult
+         * \return
+         *
+         * Types handled include: integers, floats
+         */
         QVariant resultShortToVariant(const BasePacketPtr aPacket, const Result &aResult);
 
         QVariant resultHexStringToVariant(const BasePacketPtr aPacket, const Result &aResult);
@@ -178,8 +220,12 @@ namespace DS2PlusPlus {
 
         static char decode_vin_char(int start, const QByteArray &bytes);
 
+        /*!
+         * \brief runRpnForResult runs the RPN calculations listed for a given result.
+         */
         template <typename X> static X runRpnForResult(const Result &aResult, X aValue);
 
+    /*! \cond internal */
     protected:
         quint32 _dppVersion;
         quint32 _fileVersion;
@@ -199,6 +245,7 @@ namespace DS2PlusPlus {
         static QHash<QString, QList<quint8> > _familyDictionary;
         static QHash<QString, QString> _familyNames;
         static const QChar zeroPadding;
+    /*! \endcond internal */
     };
 
     typedef QSharedPointer<ControlUnit> ControlUnitPtr;
