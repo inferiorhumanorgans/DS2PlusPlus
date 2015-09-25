9cbf39efce5414f08447b7f8cfaea2508601a622
Alex Zepeda <alex@inferiorhumanorgans.com>
2015-09-25 00:06:43 -0700

    Exception: Create a common ancestor.

--- libds2/ds2/exceptions.h
+++ libds2/ds2/exceptions.h
@@ -5,12 +5,23 @@
 #include <iostream>
 
 namespace DS2PlusPlus {
-    class TimeoutException : public std::ios_base::failure {
+    /*!
+     * \brief An abstract class for DS2PlusPlus exceptions to inherit.
+     */
+    class Exception {};
+
+    /*!
+     * \brief An exception thrown when a timeout condition is encountered while reading or writing to the serial port.
+     */
+    class TimeoutException : public Exception, public std::ios_base::failure {
     public:
         TimeoutException() : std::ios_base::failure("Timeout") {}
     };
 
-    class CommandlineArgumentException : public std::invalid_argument {
+    /*!
+     * \brief An exception thrown when a command line argument is inapproprite.
+     */
+    class CommandlineArgumentException : public Exception, public std::invalid_argument {
     public:
         CommandlineArgumentException(std::string s) : std::invalid_argument(s) {}
     };
