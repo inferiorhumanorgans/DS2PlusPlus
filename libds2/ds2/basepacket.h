c151adc4171c13b1f1c443c57a596799167dc8d2
Alex Zepeda <alex@inferiorhumanorgans.com>
2015-10-03 13:14:21 -0700

    Documentation updates.

--- libds2/ds2/basepacket.h
+++ libds2/ds2/basepacket.h
@@ -13,17 +13,23 @@ namespace Json {
 
 namespace DS2PlusPlus {
     /*!
-     * \brief The DS2Response class encapsulates a parsed response from an ECU.
+     * \brief The PacketResponse class encapsulates a parsed response from an ECU.
      */
     typedef QHash<QString, QVariant> PacketResponse;
     const Json::Value *ResponseToJson(const PacketResponse &aResponse);
     const QString ResponseToJsonString(const PacketResponse &aResponse);
     const QString HashToJsonString(const PacketResponse &aResponse, const QString &aRootNode = QString::null);
 
+    /*!
+     * \brief The BasePacket class is an abstract class representing a data packet from a ControlUnit.
+     */
     class BasePacket : public QObject
     {
         Q_OBJECT
     public:
+        /*!
+          * \brief The protocol used by a packet object.  Most supported modules use BMW's DS2 protocol, while Bosch Motronic units appear to use a KWP like protocol.
+          */
         typedef enum {
             ProtocolNone    = 0x00,
             ProtocolDS2     = 0x01,
@@ -31,6 +37,10 @@ namespace DS2PlusPlus {
             ProtocolUnknown = 0x04
         } ProtocolType;
 
+        /*!
+         * \brief Constructs an empty BasePacket.
+         * \param parent
+         */
         explicit BasePacket(QObject *parent = 0);
 
         /*!
@@ -49,10 +59,19 @@ namespace DS2PlusPlus {
          */
         BasePacket(quint8 aTargetAddress, quint8 aSourceAddress, const QByteArray &someData, QObject *aParent = 0);
 
+        /*!
+         * \brief prettyPrintPartNumber takes a seven character string and adds BMW-style delimiters to it
+         * \param aPartNumber A string representing the unformated, short part number (ex: 1234567)
+         * \return A formatted QString representing the delimited part number (ex: 1.234.567)
+         */
         static QString prettyPrintPartNumber(const QString &aPartNumber);
 
         quint8 targetAddress() const;
         void setTargetAddress(quint8 anAddress);
+
+        /*!
+         * \brief The target address of a packet.  The DS2 protocol doesn't make a source/target address distinction, only KWP does.
+         */
         Q_PROPERTY(quint8 targetAddress MEMBER _targetAddress READ targetAddress WRITE setTargetAddress)
 
         bool hasSourceAddress() const {return _hasSourceAddress;}
@@ -77,17 +96,26 @@ namespace DS2PlusPlus {
 
         virtual QByteArray expectedHeaderPadding() const = 0;
 
+        /*!
+         * \brief operator QByteArray turns a packet object into a QByteArray suitable for serializing.
+         */
         operator QByteArray() const;
 
+        /*!
+         * \brief HEX_CHAR_FORMAT The printf format string we use to format base-16 integers for human consumption
+         */
         static const char *HEX_CHAR_FORMAT;
 
     protected:
         virtual const QByteArray toByteArray() const = 0;
 
+        /*! \cond internal */
         bool _hasSourceAddress;
         quint8 _sourceAddress, _targetAddress;
         QByteArray _data;
+        /*! \endcond internal */
     };
+
     typedef QSharedPointer<BasePacket> BasePacketPtr;
 }
 
